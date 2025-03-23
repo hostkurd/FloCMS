46bc58172adc0796a44e9dcb1b0c878b49b54bad
Khalat J. Fayaq <khalat.jalal@hotmail.com>
2025-03-23 17:46:09 +0300

    ddd

--- vendor/flo/flo-cli/src/Core.php
+++ vendor/flo/flo-cli/src/Core.php
@@ -10,12 +10,23 @@ Class Core{
         $this->FileName = $fileName;
     }
 
+    private function ControllerText(){
+        return '<?php
+class '.$this->FileName.'Controller extends Controller{
+    public function __construct(array $data = array())
+    {
+        parent::__construct($data);
+        $this->model = new PagesModel();
+    }
+
+    public function index(){ 
+        $this->data[\'title\'] = \'Pages Index Title\';
+    }
+}';
+    }
     public function CreateController(){
         $file = fopen('controllers/' . $this->FileName .".controller.php", "w") or die("Unable to open file!");
-        $txt = "John Doe\n";
-        fwrite($file, $txt);
-        $txt = "Jane Doe\n";
-        fwrite($file, $txt);
+        fwrite($file, $this->ControllerText());
         fclose($file);
     }
 }
