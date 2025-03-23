0e0720bf9a40d9d669817a4815f4aa2b5ad578b8
Khalat J. Fayaq <khalat.jalal@hotmail.com>
2025-03-23 11:06:25 +0300

    Gate Command

---
+++ vendor/flo/flo-cli/src/Gate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Gate Class for FLO-CLI
+ */
+final class Gate
+{
+    private $Command;
+
+    public function __construct($data){
+        $this->Command = $data;
+    }
+
+    public function Execute($Command){
+        
+    }
+
+
+    
+}
