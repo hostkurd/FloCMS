1e191e7ea326aee5790dce0b9816d48fc30035ad
Khalat J. Fayaq <khalat.jalal@hotmail.com>
2025-03-23 22:29:23 +0300

    dev p6

--- vendor/flo/flo-cli/src/Gate.php
+++ vendor/flo/flo-cli/src/Gate.php
@@ -11,23 +11,28 @@ final class Gate
     }
 
     public function Execute(){
+        $num_params = count($this->Command);
+        if($num_params<2){
+            return "\033[32m"."Flo CLI "."\033[37m"."Version 1.0\r\nFloCMS Command line interface.";
+        }
+        
         switch($this->Command[1]){
             case '--v':
                 return 'Version 1.8';
                 break;
-            case 'Make:Model':
+            case 'create:Model':
                 return 'Create Model Command';
                 break;
             case '--help':
                 $Help = new Help();
                 return print_r($Help -> HelpData(), true);
                 break;
-            case 'make:controller':
+            case 'create:controller':
                 $Core = new Core($this->Command[2]);
                 return $Core -> CreateController();
                 break;
             default:
-                return 'Command not valid';
+                return "Command not valid \r\nRun \"php flo --help\" to view available FloCMS Commands.";
                 break;
         }
     }
--- vendor/flo/flo-cli/src/Help.php
+++ vendor/flo/flo-cli/src/Help.php
@@ -13,8 +13,10 @@ Class Help{
         $array =  [
             new Help ("--v\t\t", "Displays the Version of the FLO Framework."),
             new Help ("--help\t\t", "Displays the available Commands of Flo-CLI."),
-            new Help ("make:model\t", "Creates model element in the script"),
-            new Help ("make:controller\t", "Create Controller Class for the Object")
+            new Help ("create:model\t", "Creates model element in the script"),
+            new Help ("create:controller\t", "Create Controller Class for the Object."),
+            new Help ("create:view\t", "Create View template for the Object."),
+            new Help ("create:route\t", "Creates all the required elements or the route such as (Controler, model and view).")
         ];
 
         echo "Options:\r\n";
