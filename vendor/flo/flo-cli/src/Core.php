d690a770e32dce0d876fcd0c5e19ccb3d81fc75b
Khalat J. Fayaq <khalat.jalal@hotmail.com>
2025-11-15 08:43:33 +0300

    v1.1.3

--- vendor/flo/flo-cli/src/Core.php
+++ vendor/flo/flo-cli/src/Core.php
@@ -24,14 +24,14 @@ Class Core{
         }
         $fileName = $this->Command[2];
         //echo __DIR__;
-        $text = sprintf($this->TextData['controller-text'],ucfirst($fileName));
-        // print_r($this->TextData['controller-text']);
+        $text = sprintf($this->TextData['controller-text'],ucfirst($fileName),ucfirst($fileName));
         
-         $file = fopen('controllers/' . $fileName .".controller.php", "w") or die("Unable to open file!");
-         if(fwrite($file, $text)){
-             return $this->Colors->getColoredString('Info:', 'white','blue')."Controller has been Created Successfully.";
-             fclose($file);
-         };
+         $path = 'controllers/' . $fileName .'.controller.php';
+         if($this->CreateFile($path, $text)){
+            return $this->Colors->getColoredString('Info:', 'white','blue')."Controller has been Created Successfully.";
+         }else{
+            return $this->Colors->getColoredString('Warning:', 'white','red')."Controller already Exist.";
+         }
         
     }
 
@@ -44,30 +44,101 @@ Class Core{
         $text = sprintf($this->TextData['model-text'],ucfirst($fileName));
         // print_r($this->TextData['controller-text']);
         
-         $file = fopen('models/' . $fileName .".model.php", "w") or die("Unable to open file!");
-         if(fwrite($file, $text)){
-             return $this->Colors->getColoredString('Info:', 'white','blue')."Model has been Created Successfully.";
-             fclose($file);
-         };
+         //$file = fopen('models/' . $fileName .".model.php", "w") or die("Unable to open file!");
+         $path = 'models/' . $fileName .'.model.php';
+         if($this->CreateFile($path, $text)){
+            return $this->Colors->getColoredString('Info:', 'white','blue')."Model has been Created Successfully.";
+         }else{
+            return $this->Colors->getColoredString('Warning:', 'white','red')."Model already Exist.";
+         }
     }
 
-    public function CreateView(){
+    public function CreateView($fileName = null){
         if(!isset($this->Command[2])){
             return $this->Colors->getColoredString('Warning:', 'white','red') . "View name and route is required, please enter valid View name with the command.";
         }
 
         if($this->Command[2] == '--help'){
             $title = $this->Colors->getColoredString('How to Use Create View command:', 'green','black');
-            $text =  $this->HelpData['create-view'];
-            return sprintf($text, $title);
+            return sprintf($this->HelpData['create-view'], $title); 
         }
+
+        $controler = $this->Command[2];
 
         if(!isset($this->Command[3])){
-            return $this->Colors->getColoredString('Warning:', 'white','red') . "View route is required, please enter valid route for the View.";
+            if ($fileName == null)
+                return $this->Colors->getColoredString('Warning:', 'white','red') . "View route is required, please enter valid route for the View.";
+        }else{
+           $fileName =  $this->Command[3]; 
+        }
+        
+        $path = 'views/'.$controler.'/' . $fileName .".html";
+        $text = sprintf($this->TextData['view-text'],ucfirst($fileName));
+
+        
+
+        if($this->CreateFile($path, $text)){
+            echo $this->Colors->getColoredString('Info:', 'white','blue')."View file has been Created Successfully.";
+         }else{
+            echo $this->Colors->getColoredString('Warning:', 'white','red')."View file already Exist.";
+         }
+
+
+        $controllerPath = 'controllers/' . $controler .'.controller.php';
+        $methodText = sprintf($this->TextData['method-text'],ucfirst($fileName));  
+        try {
+            $this->AppendClassMethod($controllerPath, $methodText);
+        } catch (Exception $th) {
+            echo $th;
+        } 
+        
+        return true;
+    }
+
+    protected function CreateFile($file, $content){
+        if (file_exists($file)) {
+            return false;
+        }
+        // if file dir not exist create it
+        $fileDir = dirname($file);
+        if (!is_dir($fileDir)) mkdir($fileDir, 0755, true);
+
+        file_put_contents($file, $content);
+        return true;
+    }
+
+    protected function AppendClassMethod($filePath, $methodCode){
+        if (!file_exists($filePath)) {
+            echo "File not found: $filePath";
+            throw new Exception("File not found: $filePath");
+        }
+        // Read file
+        $content = file_get_contents($filePath);
+
+        // Find last closing bracket of the class
+        $pos = strrpos($content, "}");
+
+        if ($pos === false) {
+            throw new Exception("Invalid class file, missing closing }");
+            
         }
 
-        $fileName = $this->Command[2];
-        $route =  $this->Command[3];
+        // Insert method before the last "}"
+        $newContent =
+            substr($content, 0, $pos) .
+            "\n\n    " . trim($methodCode) . "\n" .
+            "}\n";
+
+        // Write updated file
+        file_put_contents($filePath, $newContent);
+
+        return true;
+    }
+
+    public function CreateRoute(){
+        $this->CreateController();
+        $this->CreateModel();
+        $this->CreateView('index');
     }
 
     public function ShowAllCommands():void{
--- vendor/flo/flo-cli/src/Gate.php
+++ vendor/flo/flo-cli/src/Gate.php
@@ -22,26 +22,35 @@ final class Gate
             echo "Flo CLI Version 1.0\nFloCMS Command line interface.";
         }
         
-        switch($this->Command[1]){
-            case '--v':
-                echo 'Version 1.8';
-                break;
-            case '--help':
-                echo $Core -> ShowAllCommands();
-                break;
-            case 'create:controller':
-                echo $Core -> CreateController();
-                break;
-            case 'create:model':
-                echo $Core -> CreateModel();
-                break;
-            case 'create:view':
-                echo $Core -> CreateView();
-                break;
-            default:
-                echo "Command not valid \r\nRun \"php flo --help\" to view available FloCMS Commands.";
-                break;
+        if (isset($this->Command[1])){
+            switch($this->Command[1]){
+                case '--v':
+                    echo 'Version 1.8';
+                    break;
+                case '--help':
+                    echo $Core -> ShowAllCommands();
+                    break;
+                case 'create:controller':
+                    echo $Core -> CreateController();
+                    break;
+                case 'create:model':
+                    echo $Core -> CreateModel();
+                    break;
+                case 'create:view':
+                    echo $Core -> CreateView();
+                    break;
+                case 'create:route':
+                    echo $Core -> CreateRoute();
+                    break;
+                default:
+                    echo "Command not valid \r\nRun \"php flo --help\" to view available FloCMS Commands.";
+                    break;
+            }
+        }else{
+            echo "\r\n \r\n";
+            echo $Core -> ShowAllCommands();
         }
+
     }
     
 }
